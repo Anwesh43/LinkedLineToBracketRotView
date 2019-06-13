a1d2078fb9f597a28325f08d912ca7d853789b6f
Anwesh43 <anweshthecool0@gmail.com>
2019-06-13 10:48:03 +0530

    adding LTBRNode class

--- linetobracketrotview/src/main/java/com/anwesh/uiprojects/linetobracketrotview/LineToBracketRotView.kt
+++ linetobracketrotview/src/main/java/com/anwesh/uiprojects/linetobracketrotview/LineToBracketRotView.kt
@@ -130,4 +130,48 @@ class LineToBracketRotView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LTBRNode(var i : Int, val state : State = State()) {
+
+        private var next : LTBRNode? = null
+        private var prev : LTBRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = LTBRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawLTBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LTBRNode {
+            var curr : LTBRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
