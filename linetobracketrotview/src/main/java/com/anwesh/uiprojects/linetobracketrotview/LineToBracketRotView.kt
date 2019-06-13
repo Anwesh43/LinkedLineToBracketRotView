a64a2ee83573be3ee3e30d4bb165ccfdaf04405b
Anwesh43 <anweshthecool0@gmail.com>
2019-06-13 09:26:44 +0530

    adding constants and libraries

--- linetobracketrotview/src/main/java/com/anwesh/uiprojects/linetobracketrotview/LineToBracketRotView.kt
+++ linetobracketrotview/src/main/java/com/anwesh/uiprojects/linetobracketrotview/LineToBracketRotView.kt
@@ -4,3 +4,20 @@ package com.anwesh.uiprojects.linetobracketrotview
  * Created by anweshmishra on 13/06/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 2
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val foreColor : Int = Color.parseColor("#0D47A1")
+val backColor : Int = Color.parseColor("#BDBDBD")
+val rotDeg : Float = 90f
