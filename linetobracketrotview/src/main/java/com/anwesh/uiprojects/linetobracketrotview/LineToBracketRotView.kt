e21cc6a266f0edbba26d2bbb529adf0b0e7b6fd7
Anwesh43 <anweshthecool0@gmail.com>
2019-06-13 10:28:42 +0530

    adding Canvas extension method

--- linetobracketrotview/src/main/java/com/anwesh/uiprojects/linetobracketrotview/LineToBracketRotView.kt
+++ linetobracketrotview/src/main/java/com/anwesh/uiprojects/linetobracketrotview/LineToBracketRotView.kt
@@ -21,6 +21,7 @@ val scDiv : Double = 0.51
 val foreColor : Int = Color.parseColor("#0D47A1")
 val backColor : Int = Color.parseColor("#BDBDBD")
 val rotDeg : Float = 90f
+val lFactor : Float = 4f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
@@ -31,3 +32,36 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     return (1 - k) * a.inverse() + k * b.inverse()
 }
 fun Float.updateValue(dir : Float, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawLineToBracketRot(i : Int, size : Float, sc1 : Float, sc2 : Float, paint : Paint) {
+    val sc1i : Float = sc1.divideScale(i, lines)
+    val sc2i : Float = sc2.divideScale(i, lines)
+    val sf : Float = 1f - 2 * i
+    save()
+    translate(size * sc2i * sf, 0f)
+    for (j in 0..(lines - 1)) {
+        val sci1j : Float = sc1i.divideScale(j, lines)
+        save()
+        translate(0f, -size * (1f - 2 * j))
+        rotate(rotDeg * sci1j)
+        drawLine(0f, 0f, 0f, (size / lFactor) * (1f - 2 * j), paint)
+        restore()
+    }
+    restore()
+}
+
+fun Canvas.drawLTBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    paint.color = foreColor
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    paint.strokeCap = Paint.Cap.ROUND
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    save()
+    translate(w / 2, gap * (i + 1))
+    drawLineToBracketRot(i, size, sc1, sc2, paint)
+    restore()
+}
